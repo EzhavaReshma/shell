7e020c156a44995c0435b72edb93a19b1c11d470
Reshma <ezhavareshma12@gmail.com>
2025-04-10 20:51:55 -0400

    Modified changes my removing .log

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -15,7 +15,7 @@ touch analysis/main.py
 
 # download client data
 curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
-unzip -q rawdata.zip
+unzip -qo rawdata.zip
 
 ###########################################
 # Complete assignment here
@@ -30,7 +30,7 @@ mv ./rawdata ./data/raw
 ls ./data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
+mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp ./data/raw/*server*.log ./data/processed/server_logs/
@@ -40,8 +40,8 @@ cp ./data/raw/*user*.log ./data/processed/user_logs/
 cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-#rm .data/raw/*ipaddr*.log
-#rm ./data/processed/user_logs/*ipaddr*.log
+rm .data/raw/*ipaddr*
+rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
